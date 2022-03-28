133d8f1700041a7d2906bae8678e94a8cedd094e
Dima <bycodexp@gmail.com>
2022-03-28 12:51:01 +0200

    add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
-const { src, dest, parallel, watch } = require('gulp');
-
+const { src, dest, series, parallel, watch } = require('gulp');
+const html_extend = require('gulp-html-extend');
 const postcss = require('gulp-postcss');
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
 
-const gulpHtmlExtend = require('gulp-html-extend');
+// BrowserSync
 
-function styles() {
+function browserServe(done) {
+   browserSync.init({
+      server: 'dist',
+      injectChanges: true,
+      port: 1234
+   })
+   done();
+}
+
+function reloadBrowser(done) {
+   browserSync.reload();
+   done();
+}
+
+// Tasks
+function html() {
+   return src('src/*.html')
+      .pipe(html_extend())
+      .pipe(dest('dist'))
+      .pipe(browserSync.stream());
+}
+
+function css() {
    return src('src/main.css')
       .pipe(postcss())
-      .pipe(dest('dist'));
+      .pipe(dest('dist'))
+      .pipe(browserSync.stream());
 }
 
-function views() {
-   return src('src/*.html')
-      .pipe(gulpHtmlExtend())
-      .pipe(dest('dist'));
+// Watch
+function watchTask() {
+   watch('src/**/*.{html,css}', series(html, css));
 }
 
-exports.default = parallel(styles, views);
-
-exports.watch = function () {
-   watch('src/**/*.html', views);
-   watch('src/**/*.{html,css}', styles);
-}
+exports.dev = series(browserServe, watchTask);
+exports.default = parallel(html, css);
